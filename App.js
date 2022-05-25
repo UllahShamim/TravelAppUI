b194e87408ff5eed8aac215a8c7327cae42dab4e
Shamim Ullah <shamim.ullah.86@gmail.com>
2022-05-25 17:53:15 +0100

    Assets (images, colors and fonts) created

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Lato_400Regular,
+  Lato_700Bold,
+  Lato_400Regular_Italic,
+} from '@expo-google-fonts/lato';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+    Lato_400Regular_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app! Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +33,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 32,
+    fontFamily: "Lato_400Regular",
+  }
 });
